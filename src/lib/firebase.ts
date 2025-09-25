29d9097132e80e6232a14bea59c3bfaa5f3f4c23
Riki PurPur <rikipurpur98@gmail.com>
2025-09-25 03:37:12 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "your-api-key",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "your-auth-domain",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "your-project-id",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "your-storage-bucket",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "your-messaging-sender-id",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "your-app-id",
+  apiKey: "AIzaSyCRRbWp50w9YKe-jXKEs9SJ4qkVcV0UhlI",
+  authDomain: "studio-7969231078-4753a.firebaseapp.com",
+  projectId: "studio-7969231078-4753a",
+  storageBucket: "studio-7969231078-4753a.appspot.com",
+  messagingSenderId: "193649213751",
+  appId: "1:193649213751:web:ac0c613424a1b9c34bdab2",
 };
 
 // Initialize Firebase
